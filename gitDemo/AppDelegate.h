b56d8dd7a42a407add354d99ba152ab32084d3b0
yumingde45 <402850049@qq.com>
2015-05-13 11:32:32 +0800

    new project

---
+++ gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by yumingde45 on 15-5-13.
+//  Copyright (c) 2015年 yumingde45. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by yumingde45 on 15-5-13.
+//  Copyright (c) 2015年 yumingde45. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
