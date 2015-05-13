8ccb68d61479185bbed73e3ceae0e99080b74738
yumingde45 <402850049@qq.com>
2015-05-13 11:43:04 +0800

    add control class

---
+++ gitDemo/control.h
@@ -0,0 +1,13 @@
+//
+//  control.h
+//  gitDemo
+//
+//  Created by yumingde45 on 15-5-13.
+//  Copyright (c) 2015年 yumingde45. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface control : NSObject
+
+@end
---
+++ gitDemo/control.m
@@ -0,0 +1,13 @@
+//
+//  control.m
+//  gitDemo
+//
+//  Created by yumingde45 on 15-5-13.
+//  Copyright (c) 2015年 yumingde45. All rights reserved.
+//
+
+#import "control.h"
+
+@implementation control
+
+@end
